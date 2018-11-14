3530051c3be6768d0d893a007352c50d214a3ba7
Hibabelmo <43874134+Hibabelmo@users.noreply.github.com>
2018-11-14 23:46:56 +0100

    first

--- index.php
+++ index.php
@@ -1,39 +1,82 @@
 <html>
-<body style = "text-align : centre; font-size:50 px;">
-<br>
-<?php
-if (!isset ($_GET ['op']) || !isset($_GET ['x'])|| !isset($_GET ['y'])){
-	echo "<h1> incorrect or incomplete dataaaaaaaa </h1>";
-	exit ();
+<head>
+
+<header>
+ <h2>Friend book</h2>
+</header>
+
+
+
+<style>
+
+/* Style the header */
+header {
+ background-color: #666;
+ padding: 30px;
+ text-align: center;
+ font-size: 35px;
+ color: white;
 }
+/* Style the footer */
+footer {
+ background-color: #777;
+ padding: 10px;
+ text-align: center;
+ color: white;
+}
+</style>
+</head>
 
-$x = $_GET ['x'];
-$y = $_GET ['y'];
 
-switch ($_GET ['op']){
-case 'sum' :
-	$result = $x + $y;
-	echo " <h1> $x + $y = $result </h1> ";
-	break;
-case 'subtract' :
-	$result = $x - $y;
-	echo "<h1> $x - $y = $result </h1>";
-	break;	
-case 'mult' :
-	$result = $x * $y;
-	echo "<h1> $x * $y = $result </h1>";
-	break;	
-case 'divide' :
-	if ($y == 0){
-		echo "<h1>cannot divide by  zero !</h1>";
-	}
-	else {	
-		$result = $x / $y;
-		echo "<h1> $x/$y = $result </h1>";
-		break;	
-	}
+<body>
+
+<form action="index.php" method="post">
+<br>Name: <input type="text" name="name">
+<input type="submit">
+</form>
+
+<?php
+echo "<h1>My best friends: </h1>";
+    
+echo "<ul>";
+
+$filename = 'newfile.txt';
+
+$file = fopen( $filename, "r" );
+    
+if( $file != false ) {
+    
+    while(!feof($file)){
+        
+        $friend = fgets($file);
+        if(strlen($friend) > 0) {
+            echo "<li>$friend</li>";
+        }
+    }
+}
+fclose( $file );
+    
+if (isset($_POST['name']) && strlen($_POST['name']) > 0) {
+    $name = $_POST['name'];
+    $file = fopen( $filename, "a" );
+    if($file != false){
+         echo "<b><li>$name</li></b>"; 
+         fwrite( $file, $name . "\n" );
+         fclose( $file );
+    }
+
+}
+echo "</ul>";
+if (isset($_POST['delete'])) {
+    $indexToBeRemoved = $_POST['delete'];
+    unset($friendsArray[$indexToBeRemoved]);
+    $friendsArray = array_values($friendsArray);
 }
 ?>
-</body>
-</html>
 
+</body>
+
+<footer>
+ <p>Footer</p>
+</footer>
+</html>
